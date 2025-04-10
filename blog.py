e639fea677e6ac143d36c9d5f68e621c4dd26cb5
ItzRealMee <itztherealmee@outlook.com>
2025-04-10 13:12:42 -0600

    dawsdw

---
+++ blog.py
@@ -0,0 +1,336 @@
+import hashlib
+import html
+import json
+import os
+import base64
+import time
+from datetime import datetime
+from multiprocessing import Lock
+
+import markdown
+
+import helpers
+from github import get_user_data_from_request
+
+blog_directory = 'blog_posts'
+
+
+class BlogPost:
+    def __init__(
+            self,
+            title: str,
+            summary: str,
+            date: str,
+            content: str,
+            url_name: str = None,
+            hash: str = None,  # noqa
+            image: str = None,
+            is_latest: bool = False,
+            co_authors: str = None
+    ):
+        if not title or not summary or not date or not content:
+            raise ValueError("Missing required fields")
+        self.title = title
+        self.url_name = url_name or title.lower().replace(" ", "-")
+        self.summary = summary
+        self.date = date
+        self.image = image
+        self.hash = hash or self._get_hash()
+        self.is_latest = is_latest
+        self._content_md = content
+        self.content = self._render_markdown()
+        if co_authors:
+            self.co_authors = [markdown.markdown(author.strip()) for author in co_authors.split(",")]
+
+        if not os.path.exists(self._get_comments_directory()):
+            os.makedirs(self._get_comments_directory())
+
+        self._comments_lock = Lock()
+        self._cached_comments: [Comment] or None = None
+        self._comments_needs_update = True
+
+    def _render_markdown(self):
+        return markdown.markdown(self._content_md, extensions=['fenced_code', 'codehilite', 'extra'])
+
+    def _get_comments_directory(self):
+        return os.path.join(blog_directory, 'comments', self.url_name)
+
+    def _get_hash(self):
+        digest = hashlib.md5(self.url_name.encode()).hexdigest()
+        base64_hash = base64.b64encode(digest.encode()).decode()
+        truncated_hash = base64_hash[:4]  # 64 ^ 4 = 16,777,216 possible hashes, should be enough
+        return truncated_hash
+
+    def mark_comments_for_update(self):
+        with self._comments_lock:
+            self._comments_needs_update = True
+
+    def _load_comments_from_disk(self):
+        directory = self._get_comments_directory()
+        comment_map: {int: Comment} = {}
+
+        for filename in os.listdir(directory):
+            if not filename.endswith('.json') or not os.path.isfile(os.path.join(directory, filename)):
+                continue
+
+            with open(os.path.join(directory, filename), encoding='utf-8', errors='ignore') as f:
+                try:
+                    data = json.load(f)
+                    comment_id = int(filename.split('.')[0])
+                    comment = Comment(**data, comment_id=comment_id)
+                    comment_map[comment_id] = comment
+                except (json.JSONDecodeError, TypeError) as e:
+                    print(f"Error loading comment from {filename}: {e}")
+                    continue
+
+        for comment in comment_map.values():
+            if comment.replies_to_id:
+                comment.replies_to = comment_map.get(comment.replies_to_id)
+
+        comments = list(comment_map.values())
+        comments.sort(key=lambda x: x.timestamp)
+        return comments
+
+    def get_comments(self):
+        with self._comments_lock:
+            if self._comments_needs_update:
+                self._cached_comments = self._load_comments_from_disk()
+                self._comments_needs_update = False
+        return self._cached_comments
+
+    def get_comment(self, comment_id: int):
+        comments = self.get_comments()
+        return next((c for c in comments if c.comment_id == comment_id), None)
+
+    def add_comment(self, user_name: str, user_id: int, comment: str, replies_to: int = None):
+        with self._comments_lock:
+            comment_id = 0
+            directory = self._get_comments_directory()
+            for filename in os.listdir(directory):
+                if not filename.endswith('.json') or not os.path.isfile(os.path.join(directory, filename)):
+                    continue
+                comment_id = max(comment_id, int(filename.split('.')[0]))
+
+            comment_id += 1
+            with open(os.path.join(directory, f'{comment_id}.json'), 'w', encoding='utf-8') as f:
+                data = {
+                    'user_name': user_name,
+                    'user_id': user_id,
+                    'comment': comment,
+                    'timestamp': time.time(),
+                    'replies_to_id': replies_to,
+                }
+                f.write(json.dumps(data, indent=4))
+
+        self.mark_comments_for_update()
+        return comment_id
+
+    def _modify_comment(self, comment_id: int, update_func):
+        comment_path = os.path.join(self._get_comments_directory(), f'{comment_id}.json')
+        if not os.path.exists(comment_path):
+            return False
+        with open(comment_path, 'r+', encoding='utf-8') as f:
+            data = json.load(f)
+            update_func(data)
+            f.seek(0)
+            f.write(json.dumps(data, indent=4))
+            f.truncate()
+
+        self.mark_comments_for_update()
+        return True
+
+    def edit_comment(self, comment_id: int, new_content: str):
+        def update_func(data):
+            data['comment'] = new_content
+            data['edited_timestamp'] = int(time.time())
+
+        return self._modify_comment(comment_id, update_func)
+
+    def delete_comment(self, comment_id: int):
+        def update_func(data):
+            data['is_deleted'] = True
+            data['comment'] = ""
+            data['edited_timestamp'] = int(time.time())
+
+        return self._modify_comment(comment_id, update_func)
+
+    def __repr__(self):
+        return f'<BlogPost title="{self.title}" date="{self.date}" url_name="{self.url_name}">'
+
+
+class Comment:
+    def __init__(
+            self,
+            comment_id: int,
+            user_name: str,
+            user_id: int,
+            comment: str,
+            timestamp: int,
+            edited_timestamp: int = None,
+            is_deleted: bool = False,
+            replies_to_id: int = None
+    ):
+        self.comment_id = comment_id
+        self.user_name = user_name
+        self.user_id = user_id
+        self.timestamp = timestamp
+        self.edited_timestamp = edited_timestamp
+        self.is_deleted = is_deleted
+        self.replies_to_id = replies_to_id
+        self.replies_to = None
+        self._process_comment(comment)
+
+    def _process_comment(self, comment):
+        if self.is_deleted:
+            self.comment = "<span class='deleted-comment'>[deleted]</span>"
+            self.edited_timestamp = None
+        else:
+            self.comment = html.escape(comment).replace("\n", "<br>")
+
+        short_comment = html.escape(comment).replace("\n", " ")
+        if len(short_comment) > 100:
+            short_comment = short_comment[:100] + "..."
+        self.short_comment = short_comment
+
+    @property
+    def date_str(self):
+        return helpers.timestamp_to_relative(self.timestamp)
+
+    @property
+    def edited_date_str(self):
+        if not self.edited_timestamp:
+            return None
+        return helpers.timestamp_to_relative(self.edited_timestamp)
+
+
+def get_blog_posts():
+    blog_posts = []
+    for entry in os.scandir(blog_directory):
+        if not entry.name.endswith('.md') or not entry.is_file():
+            continue
+        with open(entry.path, encoding='utf-8', errors='ignore') as f:
+            url_name = os.path.splitext(entry.name)[0]
+
+            metadata = {}
+
+            in_metadata = True
+            while in_metadata:
+                line = f.readline().strip()
+                if line.replace("-", "").strip() == "":
+                    in_metadata = False
+                else:
+                    try:
+                        key, value = line.split(":", 1)
+                    except ValueError:
+                        raise ValueError(f"Invalid line: {line}")
+                    key = key.strip().lower().replace(" ", "_").replace("-", "_")
+                    value = value.strip()
+                    metadata[key] = value
+
+            metadata["content"] = f.read()
+            metadata["url_name"] = url_name
+            blog_posts.append(BlogPost(**metadata))
+
+    blog_posts.sort(key=lambda x: x.date, reverse=True)
+    if blog_posts:
+        blog_posts[0].is_latest = True
+    return blog_posts
+
+
+def handle_comment(blog_id, request_, blogs):
+    blog: BlogPost = next((b for b in blogs if b.url_name == blog_id), None)
+    if not blog:
+        return
+    user_data = get_user_data_from_request(request_)
+    if not user_data:
+        return
+
+    content = request_.form.get("comment")
+    content = helpers.sanitize_comment(content)
+    if not content:
+        return
+
+    replies_to = request_.form.get("replies_to")
+
+    comment_id = blog.add_comment(
+        user_name=user_data.user_name,
+        user_id=user_data.user_id,
+        comment=content,
+        replies_to=int(replies_to) if replies_to else None
+    )
+    return comment_id
+
+
+def modify_comment(blog_id, comment_id, request_, blogs: [BlogPost]):
+    comment_id = int(comment_id)
+    user_data = get_user_data_from_request(request_)
+    if not user_data:
+        return
+
+    blog: BlogPost = next((b for b in blogs if b.url_name == blog_id), None)
+    if not blog:
+        return
+    comment = blog.get_comment(comment_id)
+    if not comment or comment.is_deleted or comment.user_id != user_data.user_id:
+        return
+
+    action = request_.form.get('action')
+    if action == 'edit':
+        new_content = helpers.sanitize_comment(request_.form.get('content'))
+        if not new_content:
+            return
+        if comment.comment == new_content:
+            return  # No changes
+
+        if not blog.edit_comment(comment_id, new_content):
+            return  # Error
+
+    elif action == 'delete':
+        if not blog.delete_comment(comment_id):
+            return
+    else:
+        return
+
+    blog.mark_comments_for_update()
+
+
+def get_rss(blog_posts: [BlogPost]):
+    items = []
+    for post in blog_posts:
+        pub_date = datetime.strptime(post.date, "%Y-%m-%d").strftime("%a, %d %b %Y 00:00:00 +0000")
+        description = f"""
+            <h2>{html.escape(post.title)}</h2>
+            <p><i>{html.escape(post.summary)}</i></p>
+            {f'<img src="https://damcraft.de{post.image}" alt="{html.escape(post.title)}">' if post.image else ''}
+            {post.content}
+        """.replace("[", "&#91;").replace("]", "&#93;")
+
+        item = f"""
+        <item>
+            <title>{post.title}</title>
+            <link>https://damcraft.de/blog/{post.url_name}</link>
+            <guid isPermaLink="true">https://damcraft.de/blog/{post.url_name}</guid>
+            <pubDate>{pub_date}</pubDate>
+            <description><![CDATA[
+                {description}
+            ]]></description>
+        </item>
+        """
+        items.append(item)
+    data = f"""<?xml version="1.0" encoding="UTF-8"?>
+    <rss version="2.0">
+    <channel>
+        <title>dam's blog</title>
+        <link>https://damcraft.de</link>
+        <description>My little place to ramble and rant on the internet</description>
+        {"".join(items)}
+    </channel>
+    </rss>"""
+    return data
+
+
+if __name__ == '__main__':
+    for blog_post in get_blog_posts():
+        print(blog_post)
+        print(blog_post.content)
+        print(blog_post.hash)
---
+++ spotify.py
@@ -0,0 +1,438 @@
+import base64
+import hashlib
+import hmac
+import json
+import time
+from datetime import datetime
+from json import JSONDecodeError
+
+import requests
+from gevent import queue, lock
+import traceback
+from typing import Literal
+
+import bs4
+from gevent.timeout import Timeout
+
+import const
+from helpers import css_escape
+
+shared_event_queues = set()
+queue_lock = lock.RLock()
+
+
+class Lyrics:
+    def __init__(self, lines):
+        # sort lines by timestamp
+        self.lyrics = {}
+        for timestamp, words in lines.items():
+            self.lyrics[timestamp] = words
+        self.lyrics = dict(sorted(self.lyrics.items()))
+
+    def get_last_lyrics(self, timestamp: float):
+        # last where <= timestamp
+        latest_line = "", 0
+        for ts, line in self.lyrics.items():
+            if ts <= timestamp:
+                latest_line = line
+            else:
+                break
+        return latest_line
+
+    # def get_all_next_lyrics(self, timestamp: float):
+    #     # all where > timestamp
+    #     new = {timestamp: self.get_last_lyrics(timestamp)}
+    #     new.update({ts: line for ts, line in self.lyrics.items() if ts > timestamp})
+    #     print(new)
+    #     return new
+
+
+def build_lyrics_css(lyrics: Lyrics | None, progress: float, duration: float, is_playing: bool) -> str:
+    if lyrics is None or not is_playing:
+        return ".song-lyrics { display: none; } .song-lyrics::after { animation: none; }"
+    unique_id = str(time.time()).replace(".", "")
+    lyrics_css = [
+        f".song-lyrics::after {{ animation: lyrics{unique_id} {duration}s steps(1) forwards; ",
+        f"animation-delay: {-progress}s; }}",
+        f"@keyframes lyrics{unique_id} {{"
+    ]
+
+    for ts, line in lyrics.lyrics.items():
+        progress_percentage = ts * 100 / duration
+        lyrics_css.append(f"{progress_percentage}% {{ content: '{css_escape(line)}'; }}")
+    lyrics_css.append("}")
+
+    lyrics_css.append(".song-lyrics { display: block; }")
+    lyrics_css = "\n".join(lyrics_css)
+    return lyrics_css
+
+
+def generate_spotify_totp():
+    # thank you https://github.com/KRTirtho/spotube/commit/59f298a935c87077a6abd50656f8a4ead44bd979 <3
+    time_interval = requests.get("https://open.spotify.com/server-time").json()["serverTime"] // 30
+
+    hmac_hash = hmac.new(
+        key=b'5507145853487499592248630329347',
+        msg=time_interval.to_bytes(8, "big"),
+        digestmod=hashlib.sha1
+    ).digest()
+
+    offset = hmac_hash[-1] & 0xF
+    code = ((hmac_hash[offset] & 0x7F) << 24 |
+            (hmac_hash[offset + 1] & 0xFF) << 16 |
+            (hmac_hash[offset + 2] & 0xFF) << 8 |
+            (hmac_hash[offset + 3] & 0xFF))
+
+    return str(code % 10 ** 6).zfill(6)
+
+
+def get_access_token(current_token):
+    if current_token == "main":
+        refresh_token = const.SPOTIFY_REFRESH_TOKEN
+        client_id = const.SPOTIFY_CLIENT_ID
+        client_secret = const.SPOTIFY_CLIENT_SECRET
+    else:
+        refresh_token = const.SPOTIFY_FALLBACK_REFRESH_TOKEN
+        client_id = const.SPOTIFY_FALLBACK_CLIENT_ID
+        client_secret = const.SPOTIFY_FALLBACK_CLIENT_SECRET
+    auth = base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()
+    headers = {
+        "Authorization": f"Basic {auth}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token
+    }
+    response = requests.post("https://accounts.spotify.com/api/token", data=data, headers=headers)
+    if response.status_code == 200:
+        at = response.json().get("access_token")
+        expires_on = datetime.now().timestamp() + response.json().get("expires_in")
+        return at, expires_on
+    return None, 0
+
+
+def get_account_bearer() -> (str, int) or None:
+    req = requests.get(
+        "https://open.spotify.com/get_access_token",
+        cookies={
+            "sp_dc": const.SPOTIFY_ACCOUNT_DC
+        },
+        params={
+            "reason": "init",
+            "productType": "web-player",
+            "totpVer": 5,
+            "ts": int(time.time()) * 1000,
+            "totp": generate_spotify_totp()
+        }
+    )
+    if req.status_code == 200:
+        account_bearer_ = req.json().get("accessToken")
+        account_bearer_expires_ = req.json().get("accessTokenExpirationTimestampMs")
+        return account_bearer_, account_bearer_expires_
+    return None, 0
+
+
+def update_lyrics(track_id, retried=False) -> Lyrics | None:
+    global account_bearer, account_bearer_expires
+    if account_bearer_expires < time.time():
+        account_bearer, account_bearer_expires = get_account_bearer()
+
+    try:
+        req = requests.get(
+            f"https://spclient.wg.spotify.com/color-lyrics/v2/track/{track_id}",
+            headers={
+                "Authorization": f"Bearer {account_bearer}",
+                "app-platform": "WebPlayer",
+                'spotify-app-version': '1.2.60.334.g09ff0619',
+                "User-Agent": ""
+            },
+            params={
+                "format": "json",
+                "vocalRemoval": "false"
+            }
+        )
+        if req.status_code in (401, 403) and not retried:
+            account_bearer, account_bearer_expires = get_account_bearer()
+            return update_lyrics(track_id, retried=True)
+        json_data = req.json()
+    except (requests.exceptions.RequestException, JSONDecodeError):
+        return None
+    if req.status_code != 200:
+        return None
+    lyric_data = json_data.get("lyrics")
+    if not lyric_data:
+        return None
+    is_synced = lyric_data.get("syncType") == "LINE_SYNCED"
+    if not is_synced:
+        return None
+    line_data = lyric_data.get("lines")
+    lines = {0: "♪"}
+    for line in line_data:
+        words = line["words"]
+        if words != "♪":
+            words = "♪ " + words
+        # words = helpers.smart_split(words, 50)
+        lines[int(line["startTimeMs"]) / 1000] = words
+
+    lyrics = Lyrics(lines)
+
+    return lyrics
+
+
+def get_spotify_status(access_token):
+    try:
+        req = requests.get("https://api.spotify.com/v1/me/player/currently-playing", headers={
+            "Authorization": f"Bearer {access_token}"
+        })
+        if req.status_code == 429:
+            return {"error": {"status": 429, "retry_after": req.headers.get("Retry-After")}}
+        return req.json()
+
+    except (requests.exceptions.RequestException, JSONDecodeError):
+        return None
+
+
+def event_reader(start, skip_rest=False):
+    yield start
+    yield last_event
+    if skip_rest:
+        return
+
+    event_queue = queue.Queue(maxsize=10)
+
+    with queue_lock:
+        shared_event_queues.add(event_queue)
+
+    try:
+        while True:
+            try:
+                with Timeout(10):  # check for new events every 10 seconds
+                    event = event_queue.get()
+                    if event is None:
+                        break
+                    yield event
+            except Timeout:
+                yield ""  # keep alive
+                continue
+    except GeneratorExit:  # client disconnected
+        pass
+    finally:
+        with queue_lock:
+            shared_event_queues.discard(event_queue)
+
+
+def event_writer(event):
+    with queue_lock:
+        dead_queues = []
+
+        for q in shared_event_queues:
+            try:
+                q.put_nowait(event)
+            except queue.Full:
+                dead_queues.append(q)
+            except Exception:
+                dead_queues.append(q)
+
+        # clean up
+        for q in dead_queues:
+            shared_event_queues.discard(q)
+
+
+def fetch_spotify_preview(last_track_id: str) -> str | None:
+    try:
+        req = requests.get(f"https://open.spotify.com/embed/track/{last_track_id}")
+        if req.status_code != 200:
+            return None
+        bs = bs4.BeautifulSoup(req.text, "html.parser")
+        data = bs.find("script", {"id": "__NEXT_DATA__"}).string
+        data = json.loads(data)
+        return data["props"]["pageProps"]["state"]["data"]["entity"]["audioPreview"]["url"]
+    except (requests.exceptions.RequestException, JSONDecodeError, KeyError):
+        return None
+
+
+def spotify_status_updater():
+    global access_token, expires_on, last_event, current_token, current_lyrics, account_bearer, account_bearer_expires
+    last_state = None
+    last_track_id = None
+    last_push = 0
+    while True:
+        if expires_on < time.time():
+            access_token, expires_on = get_access_token(current_token)
+
+        try:
+            status = get_spotify_status(access_token)
+            retry_after = 3
+            if status and status.get("error", {}).get("status") == 429:
+                retry_after = int(status.get("error", {}).get("retry_after", 3))
+                if retry_after < 3:
+                    retry_after = 3
+                if retry_after > 1000:
+                    print("We are being rate limited for too long, using fallback")
+                    if current_token == "main":
+                        current_token = "fallback"
+                    else:
+                        current_token = "main"
+                    access_token, expires_on = get_access_token(current_token)
+                    continue
+
+                print(f"We are being rate limited, retrying after {retry_after} seconds")
+
+            if status is None or "error" in status:
+                data = f"""
+                <div class="overwrite-div"></div>
+                <style>
+                    .notification-content {{
+                        display: none;
+                    }}
+                    .not-playing {{
+                        display: flex;
+                    }}
+                </style>
+                """
+                event_writer(data)
+                last_event = data
+                time.sleep(retry_after)
+                continue
+
+            song_title = status["item"]["name"]
+            track_id = status["item"]["id"]
+            if track_id != last_track_id:
+                current_lyrics = None
+            artist = ", ".join([artist["name"] for artist in status["item"]["artists"]])
+            # get the first image that is at least 100x100
+            covers = status["item"]["album"]["images"]
+            cover = covers[0]["url"]  # default to the first image
+            for cover_ in covers:
+                if cover_["height"] >= 100 and cover_["width"] >= 100:
+                    cover = cover_["url"]
+            cover = cover.replace("https://i.scdn.co/image/", "/spotify-image-proxy/")
+            progress_float = status["progress_ms"] / 1000
+            progress = int(progress_float)
+            duration_float = status["item"]["duration_ms"] / 1000
+            duration = int(duration_float)
+            song_url = status["item"]["external_urls"]["spotify"]
+            is_playing = status["is_playing"]
+            delta = duration - progress
+            duration_str = f"{duration // 60}:{duration % 60:02d}"
+
+            state = (song_title, artist, cover, duration, is_playing)
+            if state == last_state and 0 <= progress - last_push <= 5:
+                if not is_playing:
+                    time.sleep(1.5)
+                    continue
+                time.sleep(0.5)
+                continue
+            last_state = state
+            last_push = progress
+
+            song_title = css_escape(song_title)
+            artist = css_escape(artist)
+
+            unique_id = str(time.time()).replace(".", "")
+            seconds_keyframes = []
+            minutes_keyframes = []
+            for i in range(11):
+                tmp_i = i
+                if not is_playing:
+                    tmp_i = 0
+                if progress + tmp_i >= duration:
+                    tmp_i = duration - progress
+                seconds_keyframes.append(
+                    f"{i * 10}% {{ counter-increment: seconds{unique_id} {(progress + tmp_i) % 60}; }}")  # noqa
+                minutes_keyframes.append(
+                    f"{i * 10}% {{ counter-increment: minutes{unique_id} {(progress + tmp_i) // 60}; }}")  # noqa
+            seconds_keyframes = "\n".join(seconds_keyframes)
+            minutes_keyframes = "\n".join(minutes_keyframes)
+
+            data = f"""
+            <div class="overwrite-div"></div>
+            <a href="{song_url}" class="open-song" target="_blank"><div><img src="/assets/open.svg" alt="Open"></div></a>
+
+            <style>
+            .notification-content {{
+                display: flex;
+            }}
+            .not-playing {{
+                display: none;
+            }}
+
+            .song-length::before {{
+                content: "{duration_str}";
+            }}
+
+            .seconds-progress::before {{
+                content: "0" counter(seconds{unique_id});
+                animation: countSeconds{unique_id} 10s steps(10) forwards;
+            }}
+            @keyframes countSeconds{unique_id} {{
+                {seconds_keyframes}
+            }}
+            .minutes-progress::before {{
+                content: "0" counter(minutes{unique_id});
+                animation: countMinutes{unique_id} 10s steps(10) forwards;
+            }}
+            @keyframes countMinutes{unique_id} {{
+                {minutes_keyframes}
+            }}
+
+            .song-title::before {{
+                content: "{song_title}";
+            }}
+            .song-artist::before {{
+                content: "{artist}";
+            }}
+            .album-cover {{
+                background-image: url({cover});
+            }}
+            .progress-bar::before {{
+                width: {progress * 100 / duration}%;
+                animation: progress{unique_id} {delta}s forwards;
+            }}
+            """
+            if is_playing:
+                data += f"""
+                @keyframes progress{unique_id} {{
+                    to {{
+                        width: 100%;
+                    }}
+                }}
+
+                .paused {{
+                    visibility: hidden;
+                }}
+                """
+            else:
+                data += """
+                .paused {
+                    visibility: visible;
+                }
+                """
+
+            if last_track_id == track_id:
+                data += build_lyrics_css(current_lyrics, progress_float, duration_float, is_playing)
+
+            data += "</style>"
+
+            event_writer(data)
+            if last_track_id != track_id:
+                last_track_id = track_id
+                current_lyrics = update_lyrics(last_track_id)
+                new_lyrics_css = build_lyrics_css(current_lyrics, progress_float, duration_float, is_playing)
+                new_lyrics_css = "<style>" + new_lyrics_css + "</style>"
+                event_writer(new_lyrics_css)
+                last_event += new_lyrics_css
+
+            last_event = data
+        except BaseException:
+            traceback.print_exc()
+            pass
+        time.sleep(1.5)
+
+
+access_token, expires_on = get_access_token("main")
+account_bearer, account_bearer_expires = get_account_bearer()
+current_token: Literal["main", "fallback"] = "main"
+last_event = ""
+current_lyrics: Lyrics | None = None
